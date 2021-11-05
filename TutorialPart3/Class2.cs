91ee18afc0bb4c4bff5c7fc990be565bbfe2cded
Avramescu Alexandra <avramescu.alexandra@gmail.com>
2021-11-05 18:30:18 +0200

    Tutorials from 18 to 20

---
+++ TutorialPart3/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{	
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+
+	public string GetFullName() 
+	{
+		return FirstName + " " + LastName;
+	}
+	
+}
---
+++ TutorialPart3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TutorialPart3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.FirstName = "Alexandra";
+            class1.LastName = "Avramescu";
+            Console.WriteLine(class1.FirstName + " " + class1.LastName);
+
+            Console.WriteLine(class1.GetFullName());
+
+
+        }
+    }
+}
